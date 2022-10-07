11de2105a6a9f3b60d846e6fab90892b9abfa006
IslamAhmd <deveislam95@gmail.com>
2022-10-07 15:04:17 +0300

    step15: hide your secret passwords

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+
+return [
+
+    'database' => [
+
+        'connection' => 'mysql:host=localhost;',
+
+        'dbname' => 'dbname=PHP-Practitioner;',
+
+        'username' => 'root',
+
+        'password' => '51215',
+
+        'options' => [
+
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+
+        ]
+
+    ]
+
+];
--- database/Connection.php
+++ database/Connection.php
@@ -6,12 +6,20 @@ class Connection
 
     // static means you don't have to instantiate the class, so you can use the function like this
     // Connection::make()
-    public static function make()
+    public static function make($config)
     {
 
         try {
 
-            return new PDO('mysql:host=localhost;dbname=PHP-Practitioner;', 'root', '51215');
+            return new PDO
+            (
+
+                $config['connection'] . $config['dbname'],
+                $config['username'],
+                $config['password'],
+                $config['options']
+
+            );
 
         } catch (PDOException $e) {
 
